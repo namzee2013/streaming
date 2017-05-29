f05fcbd3a9439db64a8626849d3579e67257f658
Truong Thanh Nam <nam.tt@neo-lab.vn>
2017-05-29 11:23:49 +0700

    deploy

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.set('views','./views');
 app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
 
-app.listen(3000, () => console.log('server started'));
+app.listen(process.env.PORT || 3000, () => console.log('server started'));
